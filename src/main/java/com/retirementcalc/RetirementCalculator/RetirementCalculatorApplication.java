7acc80023c78d7dd6d08002f2a1c1ddbe82e8969
alvarezm3 <alvarezm3@carleton.edu>
2022-08-11 20:00:17 -0500

    Initial Code

---
+++ src/main/java/com/retirementcalc/RetirementCalculator/RetirementCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.retirementcalc.RetirementCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetirementCalculatorApplication.class, args);
+	}
+}
