16164ab8c2c2e65a42779167fcdf86abbca6901a
Miguel Alvarez <88111277+miguel-alv1@users.noreply.github.com>
2023-12-10 17:52:27 -0600

    Add files via upload
    Initial files as generated by Spring Initialzr for new Spring project

---
+++ RetirementCalculator/src/main/java/com/finance/RetirementCalculator/RetirementCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.finance.RetirementCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetirementCalculatorApplication.class, args);
+	}
+
+}
---
+++ RetirementCalculator/src/test/java/com/finance/RetirementCalculator/RetirementCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.RetirementCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetirementCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
