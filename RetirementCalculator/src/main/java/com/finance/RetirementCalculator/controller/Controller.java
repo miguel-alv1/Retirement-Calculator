302335037a1afb05f1ab02c56c7bb51da3463fd6
miguel-alv1 <miguel.alvarez9724@gmail.com>
2023-12-14 20:32:29 -0600

    configuration work, groundwork

---
+++ RetirementCalculator/src/main/java/com/finance/RetirementCalculator/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.finance.RetirementCalculator.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome!";
+    }
+}
--- RetirementCalculator/src/main/java/com/finance/RetirementCalculator/entity/Assets.java
+++ RetirementCalculator/src/main/java/com/finance/RetirementCalculator/entity/Assets.java
@@ -1,9 +1,59 @@
+package com.finance.RetirementCalculator.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="customer")
 public class Assets {
     // fields
+    @Id
+    @Column(name="customer_id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="age")
+    private String age;
+
+    @Column(name="stocks_value")
     private int stocksVal;
+
+    @Column(name="alt_assets_value")
     private int altAssetsVal;
+
+    @Column(name="real_estate_value")
     private int realEstate;
+
+    @Column(name="cash_holdings")
     private int cash;
+
+    @Column(name="total_networth")
+    private int networth;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="city")
+    private String city;
+
+    @Column(name="zip")
+    private String zip;
+
+    @Column(name="state")
+    private String state;
+
+    @Column(name="country")
+    private String country;
 
     // empty constructor
     public Assets() {
