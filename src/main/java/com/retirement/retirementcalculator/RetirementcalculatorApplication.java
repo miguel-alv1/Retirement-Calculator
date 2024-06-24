ec3ffbcab3ed7ed4da57825b2f8fa75f8b32890a
miguel-alv1 <miguel.alvarez9724@gmail.com>
2024-06-23 20:23:16 -0500

    adding untracked files

---
+++ src/main/java/com/retirement/retirementcalculator/RetirementcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.retirement.retirementcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetirementcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetirementcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retirement/retirementcalculator/RetirementcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retirement.retirementcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetirementcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
